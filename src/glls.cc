8fdaa9bf95898939a3ffe46dbfe9db2d97818747
Chuanren Wu <wuchuanren@gmail.com>
2015-04-13 10:06:42 +0200

    fix but & add arrangeY

--- src/glls.cc
+++ src/glls.cc
@@ -29,8 +29,10 @@ void arrangeX(
         coef[cols*(row+1) - 1] = g.coef[origCols*(row+1) - 1];
     }
     int origCol = 0;
-    for (int i = 0; i < xs.size(); ++i) {
+    for (std::size_t i = 0; i < xs.size(); ++i) {
         const auto &x = xs[i];
+        assert(x.first >= origCol);
+        assert(x.first < g.xSize);
         for (; origCol<x.first; ++origCol) {
             for (int row = 0; row < rows; ++row) {
                 coef[cols*row + origCol - i] = g.coef[origCols*row + origCol];
@@ -42,6 +44,35 @@ void arrangeX(
         g.reservedX[i] = x.first;
         ++origCol;
     }
+    for (; origCol<g.xSize; ++origCol) {
+        for (int row = 0; row < rows; ++row) {
+            coef[cols*row + origCol - xs.size()]
+                    = g.coef[origCols*row + origCol];
+        }
+    }
     g.coef = std::move(coef);
     g.xSize -= g.reservedX.size();
+}
+
+void arrangeY(
+        GllsProblem &g,
+        const std::list<std::vector<std::pair<int, double> > > &ys
+)
+{
+    assert(g.coef.size() % (g.xSize+1) == 0);
+    assert(ys.size() > 0);
+    const int cols = g.xSize + 1;
+    const int rows = ys.size();
+    std::vector<double> coef;
+    coef.reserve(rows*cols);
+    for (const auto &eq : ys) {
+        std::vector<double> c(cols);
+        for (const auto &y : eq) {
+            for (int i = 0; i < cols; ++i) {
+                c[i] += y.second * g.coef[y.first*cols + i];
+            }
+        }
+        std::copy(c.cbegin(), c.cend(), std::back_inserter(coef));
+    }
+    g.coef = coef;
 }
--- test/glls.cc
+++ test/glls.cc
@@ -23,7 +23,10 @@ BOOST_AUTO_TEST_SUITE()
         BOOST_REQUIRE_EQUAL(gp.xVarName(), "x");
         BOOST_REQUIRE_CLOSE(gp.xValues()[0].second, 4.0, 1e-9);
         arrangeX(g, gp.xValues());
+        BOOST_CHECK_CLOSE(g.coef[0], 2.0, 1e-9);
+        BOOST_CHECK_CLOSE(g.coef[1], 3.0, 1e-9);
         BOOST_CHECK_CLOSE(g.coef[3], 4.0, 1e-9);
+        BOOST_CHECK_CLOSE(g.coef[4], 5.0, 1e-9);
         BOOST_CHECK_CLOSE(g.coef[7], 16.0, 1e-9);
     }
 
@@ -32,11 +35,26 @@ BOOST_AUTO_TEST_SUITE()
                 "x\ny\n1 2 3 4 \n 4 5 6 8\n x0=1\n x3 = -2"
         );
         GllsParser gp(ss, true);
-        GllsProblem g;
-        BOOST_REQUIRE_NO_THROW(g = gp.run());
+        GllsProblem g;;
+        BOOST_REQUIRE_NO_THROW(g = gp.run())
         arrangeX(g, gp.xValues());
         BOOST_CHECK_CLOSE(g.coef[2], -7.0, 1e-9);
         BOOST_CHECK_CLOSE(g.coef[5], -12.0, 1e-9);
+    }
+
+    BOOST_AUTO_TEST_CASE(SolveY_1) {
+        std::istringstream ss(
+                "x\ny\n1 2 3 4 \n 4 5 6 8\n 2*y0 = y1"
+        );
+        GllsParser gp(ss, true);
+        GllsProblem g;
+        BOOST_REQUIRE_NO_THROW(g = gp.run());
+        arrangeX(g, gp.xValues());
+        arrangeY(g, gp.yConds());
+        BOOST_CHECK_CLOSE(g.coef[0], -2.0, 1e-9);
+        BOOST_CHECK_CLOSE(g.coef[1], -1.0, 1e-9);
+        BOOST_CHECK_CLOSE(g.coef[2], 0.0, 1e-9);
+        BOOST_CHECK_CLOSE(g.coef[4], 0.0, 1e-9);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
