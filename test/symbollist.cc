b6aa30475ae4b7fff7f7da7c287f56cbb0980af0
Chuanren Wu <wuchuanren@gmail.com>
2015-04-14 11:28:51 +0200

    fix test

--- test/symbollist.cc
+++ test/symbollist.cc
@@ -13,40 +13,40 @@ BOOST_AUTO_TEST_SUITE()
 
     BOOST_AUTO_TEST_CASE(EmptySymbol) {
         SymbolList l;
-        BOOST_ASSERT(!l.insert(""));
-        BOOST_ASSERT(l.query("") < 0);
+        BOOST_REQUIRE(!l.insert(""));
+        BOOST_REQUIRE(l.query("") < 0);
     }
 
     BOOST_AUTO_TEST_CASE(Duplication) {
         SymbolList l;
-        BOOST_ASSERT(l.insert("123"));
-        BOOST_ASSERT(l.insert("abc"));
-        BOOST_ASSERT(!l.insert("abc"));
-        BOOST_ASSERT(l.insert("def"));
-        BOOST_ASSERT(l.query("def") == 2);
+        BOOST_REQUIRE(l.insert("123"));
+        BOOST_REQUIRE(l.insert("abc"));
+        BOOST_REQUIRE(!l.insert("abc"));
+        BOOST_REQUIRE(l.insert("def"));
+        BOOST_REQUIRE(l.query("def") == 2);
     }
 
     BOOST_AUTO_TEST_CASE(GeneralTest) {
         SymbolList l;
-        BOOST_ASSERT(!l.insert(""));
-        BOOST_ASSERT(l.insert("abc"));
-        BOOST_ASSERT(!l.insert(""));
-        BOOST_ASSERT(l.insert("d"));
-        BOOST_ASSERT(l.insert("e"));
-        BOOST_ASSERT(l.insert("!"));
+        BOOST_REQUIRE(!l.insert(""));
+        BOOST_REQUIRE(l.insert("abc"));
+        BOOST_REQUIRE(!l.insert(""));
+        BOOST_REQUIRE(l.insert("d"));
+        BOOST_REQUIRE(l.insert("e"));
+        BOOST_REQUIRE(l.insert("!"));
         BOOST_CHECK_EQUAL(l.query("abc"), 0);
         BOOST_CHECK_EQUAL(l.query("!"), 3);
     }
 
     BOOST_AUTO_TEST_CASE(CaseSensitive) {
         SymbolList l;
-        BOOST_ASSERT(!l.insert(""));
-        BOOST_ASSERT(l.insert("abc"));
-        BOOST_ASSERT(!l.insert(""));
-        BOOST_ASSERT(l.insert("d"));
-        BOOST_ASSERT(l.insert("aBc"));
-        BOOST_ASSERT(l.insert("!"));
-        BOOST_ASSERT(l.query("abC") < 0);
+        BOOST_REQUIRE(!l.insert(""));
+        BOOST_REQUIRE(l.insert("abc"));
+        BOOST_REQUIRE(!l.insert(""));
+        BOOST_REQUIRE(l.insert("d"));
+        BOOST_REQUIRE(l.insert("aBc"));
+        BOOST_REQUIRE(l.insert("!"));
+        BOOST_REQUIRE(l.query("abC") < 0);
         BOOST_CHECK_EQUAL(l.query("aBc"), 2);
     }
 
